d6530a2dd9b31cd70c9f3aa7f95511a516021d93
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-25 17:16:17 -0700

    [js] Add startGame function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
+let gameStarted = false;
 let movementDisplay;
 let ctx;
 let game;
 let cactus;
 let enemy;
+
+const pressStart = document.querySelector('#press-start');
 
 function Crawler(x, y, width, height, color) {// color will become sprite
     this.x = x;
@@ -11,22 +14,34 @@ function Crawler(x, y, width, height, color) {// color will become sprite
     this.height = height;
     this.color = color;
     this.alive = true;
-    this.render = function () {
+    this.render = function () { // constructor function
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
-const movementHandler = (event) => {
+const startGame = (event) => {
     console.log(event);
+    console.log('game started!')
+    gameStarted = true;
+    pressStart.style.display = 'none';
+}
+
+const movementHandler = (event) => {
+    
+    console.log('the game has started');
 }
 
 const gravityHandler = (event) => {
-    console.log(event);
+    console.log('key up');
 }
 
+// main event listeners
 document.addEventListener('DOMContentLoaded', () => {
     console.log('dom content loaded');
-    document.addEventListener('keydown', movementHandler);
+    document.addEventListener('keydown', startGame);
     document.addEventListener('keyup', gravityHandler);
+    
 });
+
+// first time user presses space, it starts the game
