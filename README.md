2b2ad7c2b0c2ef50835e3c0ace75d831e2cca74e
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-26 15:57:42 -0700

    [js/readme] Add themeMusic and add to readme.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # CACTUS THE IMPALER
 A pascifistic infinite runner where you play as the fiercest warrior time has ever known: __CACTUS__ (pronounced CACK-TOOS, and don't you forget it, mortal.) __CACTUS__ has slain many foes across the many battles of his bloody career. But now, the warrior heart of __CACTUS__ has softened! He is mortified by his past deeds! He no longer longs for death and destruction! Help guide __CACTUS__ away from the battlefield while sparing the enemies who cower in fear before you. Onward __CACTUS__, to gentler times!
+
+## Technology
+For this project I utilized HTML5 Canvas, CSS, and Javascript. I started by creating the scrolling motion with the `animate()` function.
+```js
+let x = 320;
+let xVelocity = 4;
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    ctx.fillStyle = 'rgba(0, 100, 0, 1' // dark green!
+    ctx.fillRect(60, 100, 32, 32); // cactus!
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)'
+    ctx.fillRect(x, 100, 32, 32); // cowering foe!
+    x -= xVelocity; // move
+};
+```
--- js/app.js
+++ js/app.js
@@ -1,41 +1,41 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+const themeMusic = new Audio('./assets/sounds/three-red-hearts-quiet.wav')
 
-canvas.innerHeight = 440;
+
+canvas.innerHeight = 320;
 canvas.innerWidth = 480;
 
-ctx.fillStyle = 'rgba(0, 100, 0, 1';
-const cactus = ctx.fillRect(100, 100, 32, 32);
+// ctx.fillStyle = 'rgba(0, 100, 0, 1';
+// const cactus = ctx.fillRect(100, 100, 32, 32);
 
-// ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-// ctx. fillRect(200, 112, 32, 16);
 
-// BASIC SQUARE RANDOMIZER LOGIC
-// the enemies aren't random on screen
-// only generated randomly off screen
-// for (let i = 0; i < 3; i++) {
-//     let x = Math.random() * (canvas.innerWidth*2);
-//     let y = 100;
-//     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-//     ctx.fillRect(x, y, 32, 32);
-
-// }
 let x = 320;
-let xVelocity = 2.5;
+let xVelocity = 4;
 function animate() {
+    
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
-    ctx.fillStyle = 'rgba(0, 100, 0, 1'
-    ctx.fillRect(60, 100, 32, 32);
-    ctx.beginPath();
-    ctx.arc(x, 115, 15, 0, Math.PI * 2, false);
-    ctx.strokeStyle = 'black';
-    // ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.stroke();
+    ctx.fillStyle = 'rgba(0, 100, 0, 1' // dark green!
+    ctx.fillRect(60, 100, 32, 32); // cactus!
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)'
+    ctx.fillRect(x, 100, 32, 32); // cowering foe!
     x -= xVelocity; // move
-}
+};
 
-animate();
+
+
+// main event listeners
+document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('keypress', () => {
+        themeMusic.play();
+        animate();
+    });
+        
+    // document.addEventListener('keydown', startGame);
+    // document.addEventListener('keyup', gravityHandler);
+});
+
 
 // let gameStarted = false;
 // let movementDisplay;
@@ -47,6 +47,9 @@ animate();
 
 // const pressStart = document.querySelector('#press-start');
 
+const jump = () => {
+    
+}
 
 
 
@@ -88,4 +91,17 @@ animate();
 //     document.addEventListener('keyup', gravityHandler);
 // });
 
-// first time user presses space, it starts the game
+
+// ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+// ctx. fillRect(200, 112, 32, 16);
+
+// BASIC SQUARE RANDOMIZER LOGIC
+// the enemies aren't random on screen
+// only generated randomly off screen
+// for (let i = 0; i < 3; i++) {
+//     let x = Math.random() * (canvas.innerWidth*2);
+//     let y = 100;
+//     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+//     ctx.fillRect(x, y, 32, 32);
+
+// }
